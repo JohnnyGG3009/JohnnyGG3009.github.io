9e7e66d6a2d373dec58b2a05881496134514bbd2
JohnnyGG3009 <142618312+JohnnyGG3009@users.noreply.github.com>
2025-02-14 17:59:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 onload = () => {
     document.body.classList.remove("container");
-  };
+  }
 
   const wrapper = document.querySelector(".wrapper");
   const openBtn = document.getElementById("openBtn");
